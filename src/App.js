d161239e50e7f2f42c4696a62589ce6e7f3cba30
treuther <tracyreuther@gmail.com>
2021-05-01 15:01:51 -0500

    useState for submit button complete

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ const App = () => {
   // const [value, setValue] = useState(initialState);
   const [name, setName] = useState('');
 
+  // e.preventDefault prevents the form from refreshing the page.
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle />
-      <h3>{name}</h3>
+      <form onSubmit={e => {
+        e.preventDefault();
+        formSubmit(name, setName);
+      }}>
       <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+      <button>Submit</button>
+      </form>
     </div>
   );
 };
+
+const formSubmit = (value, setValue) => {
+  console.log('email sent to ' + value + '!');
+  setValue('')
+}
 
 export default App;
